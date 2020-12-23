c63cc9709d55b0b463cfee3fdf083747efff894f
David Banda <davidbandag96@gmail.com>
2020-12-23 14:02:42 -0700

    create project and update it

---
+++ excel-automation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace excel_automation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ excel-automation/wwwroot/js/filesName.js
@@ -0,0 +1,12 @@
+﻿const filesInput = document.querySelector('.custom-file-input');
+const labelFiles = document.querySelector('.custom-file-label')
+
+filesInput.addEventListener('change', (event) => {
+    if (event.target.files.length == 1) {
+        labelFiles.innerText = event.target.files.item(0).name;
+        return;
+    }
+
+    const filesNumber = event.target.files.length;
+    labelFiles.innerText = `${event.target.files.length} files(s) selected`
+})
