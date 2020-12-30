f3918cd5a6266bb4e0a9c032f6286727fa80dda7
David Banda <davidbandag96@gmail.com>
2020-12-30 10:43:51 -0700

    get value from a cell

--- excel-automation/Controllers/HomeController.cs
+++ excel-automation/Controllers/HomeController.cs
@@ -7,21 +7,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using excel_automation.Models;
 using Microsoft.AspNetCore.Http;
-using System.IO;
 using Syncfusion.XlsIO;
-using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace excel_automation.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _env = env;
         }
 
         public IActionResult Index()
@@ -41,41 +38,45 @@ namespace excel_automation.Controllers
             IApplication application = excelEngine.Excel;
 
             //Assigns default application version
-            application.DefaultVersion = ExcelVersion.Excel2016;
+            application.DefaultVersion = ExcelVersion.Xlsx;
 
             using (var sampleFile = excel[0].OpenReadStream())
             {
                 IWorkbook workbook = application.Workbooks.Open(sampleFile);
 
                 //Access first worksheet from the workbook.
-                IWorksheet worksheet = workbook.Worksheets[0];
+                IWorksheet worksheet = workbook.Worksheets[1];
 
-                //Set Text in cell A3.
-                worksheet.Range["A3"].Text = "Hello World";
+                Console.WriteLine(worksheet.Name);
+                Console.WriteLine(worksheet.Range["A2"].Value);
 
-                //Defining the ContentType for excel file.
-                string ContentType = "Application/msexcel";
+                ////Set Text in cell A3.
+                //worksheet.Range["A3"].Text = "Ejemplo de modificacion de excel";
 
-                //Define the file name.
-                string fileName = "Output.xlsx";
+                ////Defining the ContentType for excel file.
+                //string ContentType = "Application/msexcel";
 
-                //Creating stream object.
-                MemoryStream stream = new MemoryStream();
+                ////Define the file name.
+                //string fileName = "Output.xlsx";
 
-                //Saving the workbook to stream in XLSX format
-                workbook.SaveAs(stream);
+                ////Creating stream object.
+                //MemoryStream stream = new MemoryStream();
 
-                stream.Position = 0;
+                ////Saving the workbook to stream in XLSX format
+                //workbook.SaveAs(stream);
 
-                //Closing the workbook.
-                workbook.Close();
+                //stream.Position = 0;
 
-                //Dispose the Excel engine
-                excelEngine.Dispose();
+                ////Closing the workbook.
+                //workbook.Close();
 
-                //Creates a FileContentResult object by using the file contents, content type, and file name.
-                return File(stream, ContentType, fileName);
+                ////Dispose the Excel engine
+                //excelEngine.Dispose();
+
+                ////Creates a FileContentResult object by using the file contents, content type, and file name.
+                //return File(stream, ContentType, fileName);
             }
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
