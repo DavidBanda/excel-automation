8e6c0b48a4ec36dbf2f108acd76b6f315443b1b9
David Banda <davidbandag96@gmail.com>
2021-01-04 13:43:20 -0700

    reestructure the project to handle all from javascript

--- excel-automation/Controllers/HomeController.cs
+++ excel-automation/Controllers/HomeController.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using excel_automation.Models;
 using Microsoft.AspNetCore.Http;
-using Syncfusion.XlsIO;
-using System.IO;
 
 namespace excel_automation.Controllers
 {
@@ -26,58 +24,6 @@ namespace excel_automation.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Index(List<IFormFile> excel, List<IFormFile> template)
-        {
-            //New instance of ExcelEngine is created 
-            //Equivalent to launching Microsoft Excel with no workbooks open
-            //Instantiate the spreadsheet creation engine
-            ExcelEngine excelEngine = new ExcelEngine();
-
-            //Instantiate the Excel application object
-            IApplication application = excelEngine.Excel;
-
-            //Assigns default application version
-            application.DefaultVersion = ExcelVersion.Xlsx;
-
-            using (var sampleFile = excel[0].OpenReadStream())
-            {
-                IWorkbook workbook = application.Workbooks.Open(sampleFile);
-
-                //Access first worksheet from the workbook.
-                IWorksheet worksheet = workbook.Worksheets[1];
-
-                Console.WriteLine(worksheet.Name);
-                Console.WriteLine(worksheet.Range["A2"].Value);
-
-                ////Set Text in cell A3.
-                //worksheet.Range["A3"].Text = "Ejemplo de modificacion de excel";
-
-                ////Defining the ContentType for excel file.
-                //string ContentType = "Application/msexcel";
-
-                ////Define the file name.
-                //string fileName = "Output.xlsx";
-
-                ////Creating stream object.
-                //MemoryStream stream = new MemoryStream();
-
-                ////Saving the workbook to stream in XLSX format
-                //workbook.SaveAs(stream);
-
-                //stream.Position = 0;
-
-                ////Closing the workbook.
-                //workbook.Close();
-
-                ////Dispose the Excel engine
-                //excelEngine.Dispose();
-
-                ////Creates a FileContentResult object by using the file contents, content type, and file name.
-                //return File(stream, ContentType, fileName);
-            }
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
