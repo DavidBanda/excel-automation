0170adaba638d5057a8bbd2517d847aa602b4d63
David Banda <davidbandag96@gmail.com>
2021-01-05 18:57:40 -0700

    add select option to the table

--- excel-automation/wwwroot/js/manageExcelFiles.js
+++ excel-automation/wwwroot/js/manageExcelFiles.js
@@ -1,52 +1,79 @@
 ﻿const uploadButton = document.querySelector('.upload-button');
 const divTable = document.querySelector('.divTable');
+const dataTable = document.querySelector('#DT_load');
 
 filesInputExcel.addEventListener('change', handleFileSelect, false);
 filesInputTemplate.addEventListener('change', loadColumnsDT, false);
 
-//add columns to the table
-//$("#DT_load>thead>tr").append("<th>Actions</th>");
 
 function loadColumnsDT(evt) {
     divTable.hidden = false;
-    $("#DT_load>thead>tr").append("<th>name</th>");
-    $("#DT_load>thead>tr").append("<th>size</th>");
-    $("#DT_load>thead>tr").append("<th>actions</th>");
+    $('#DT_load').DataTable({
+        "scrollX": true,
+    });
+    handleFileSelect(evt);
 }
 
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-    var xl2json = new ExcelToJSON();
-    xl2json.parseExcel(files[0]);
+
+    //Validate whether File is valid Excel file.
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
+    if (regex.test(evt.target.value.toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+
+            //For Browsers other than IE.
+            if (reader.readAsBinaryString) {
+                reader.onload = function (e) {
+                    ProcessExcel(e.target.result, evt.target.className);
+                };
+                reader.readAsBinaryString(evt.target.files[0]);
+            } else {
+                //For IE Browser.
+                reader.onload = function (e) {
+                    var data = "";
+                    var bytes = new Uint8Array(e.target.result);
+                    for (var i = 0; i < bytes.byteLength; i++) {
+                        data += String.fromCharCode(bytes[i]);
+                    }
+                    ProcessExcel(data, evt.target.className);
+                };
+                reader.readAsArrayBuffer(evt.target.files[0]);
+            }
+        } else {
+            alert("This browser does not support HTML5.");
+        }
+    } else {
+        alert("Please upload a valid Excel file.");
+    }
 }
 
-var ExcelToJSON = function () {
+function ProcessExcel(data, fileInputName) {
+    //Read the Excel File data.
+    var workbook = XLSX.read(data, {
+        type: 'binary'
+    });
 
-    this.parseExcel = function (file) {
-        var reader = new FileReader();
+    if (fileInputName.includes("custom-input-template")) {
+        for (var key of workbook.Strings) {
+            $("#DT_load>thead>tr").append(`<th>${key['t']}</th>`);
+            $("#DT_load>tbody>tr").append(`<td>
+                                             <div class="col-auto mx-5">
+                                               <select class="custom-select mr-5" id="inlineFormCustomSelect">
+                                                 <option selected>Columns</option>
+                                               </select>
+                                             </div>
+                                           </td>`);
+        }
+        return;
+    }
 
-        reader.onload = function (e) {
-            var data = e.target.result;
-            var workbook = XLSX.read(data, {
-                type: 'binary'
-            });
-            workbook.SheetNames.forEach(function (sheetName) {
-                // Here is your object
+    ////Fetch the name of First Sheet.
+    var firstSheet = workbook.SheetNames[0];
 
-                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                var json_object = JSON.stringify(XL_row_object);
-                console.log(JSON.parse(json_object));
-                jQuery('#xlx_json').val(json_object);
-            })
-        };
+    //Read all rows from First Sheet into an JSON array.
 
-        reader.onerror = function (ex) {
-            console.log(ex);
-        };
+    var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
+    ////console.log(Object.keys(excelRows[0]));
 
-        reader.readAsBinaryString(file);
-    };
 };
-
-
-
